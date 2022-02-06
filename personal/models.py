17a8cda9780bdef322b6da66b19f3d7653e9b543
eun <eunmoong@gmail.com>
2022-02-06 22:11:53 +0900

    front브랜치pull

--- personal/models.py
+++ personal/models.py
@@ -4,3 +4,5 @@ from django.db import models
 
 class Image(models.Model):
     image = models.ImageField(upload_to="mediaForm/",blank=False,null=False)
+
+
--- personal/views.py
+++ personal/views.py
@@ -1,5 +1,16 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from .forms import ImageForm
+from .models import Image
 # Create your views here.
 def index(request):
     return render(request,"index.html")
+
+def imageUpload(request):
+    if request.method == 'POST': 
+        image_form = ImageForm(request.POST,request.FILES)
+        image_form.save()
+        #return redirect("colorPicker",image_form.id)
+        return redirect("index")
+    else:
+        image_form = ImageForm()
+        return render(request,'imageUpload.html',{'form':ImageForm})
