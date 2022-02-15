07d1f48651f915f6af50f2706f8104a62c8d50cc
eunyoung <eunmoong@gmail.com>
2022-02-15 17:50:57 +0900

    선택한 사진 색상픽시 뜨게 함 - 아직 세션정보는 필터 안걸었음

--- personal/views.py
+++ personal/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect,get_object_or_404
 from .forms import ImageForm
 from .models import ImageData
 from django.utils import timezone
@@ -20,13 +20,13 @@ def imageUpload(request):
             print(imgForm.sessionData)
             #만약 model에 있는 필드를 다 담아줬다면  그냥 바로 저장해줘도 됨
             imgForm.save()
-            #return redirect("detailPage",book.id)
-            return redirect("colorSelect")
+            return redirect("colorSelect",imgForm.id)
     else:
         image_form = ImageForm()
         return render(request,'imageUpload.html',{'form':ImageForm})
 
-def colorSelect(request):
+def colorSelect(request,imageId):
+    image = get_object_or_404(ImageData,pk=imageId) 
     # 세가지 숫자 조합 (0~256까지) 이중리스트로 8개 생성  
     #사진 어케 넘겨줌? \
     colorList=[]
@@ -35,4 +35,4 @@ def colorSelect(request):
         for _ in range(3):
             line.append(random.randrange(257) )
         colorList.append(line)
-    return render(request,"colorSelect.html")
+    return render(request,"colorSelect.html",{'imageContents':image})
