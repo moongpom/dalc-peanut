88d2b331cc1063509887bfb3ea174b460da1d079
eunyoung <eunmoong@gmail.com>
2022-02-25 12:52:39 +0900

    코드 수정 완2

--- peanut/settings.py
+++ peanut/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-'''
+
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
@@ -44,6 +44,7 @@ def get_secret(setting, secrets=secrets):
 
 SECRET_KEY = get_secret("SECRET_KEY")
 
+'''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
